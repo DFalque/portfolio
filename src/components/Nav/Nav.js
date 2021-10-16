de82a6d6270294a36d1439dfd1b0405569fab5fd
DFalque <falque@me.com>
2021-10-16 20:30:34 +0200

    change Nav style

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,7 @@ import "./Nav.scss"
 function Nav() {
 	return (
 		<div className="Nav">
-			<a>Portfolio</a>
+			<a className="Nav__icon">Diego Falque</a>
 			<nav className="Nav__LinksContainer">
 				<a className="Nav__LinksContainer__Link">About me</a>
 				<a className="Nav__LinksContainer__Link">Work</a>
