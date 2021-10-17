79e8b1aa26cce368dd7bf8bb1e8bd5201ed918c6
DFalque <falque@me.com>
2021-10-17 19:38:06 +0200

    add Skills section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./components/About/About"
 import Header from "./components/Header"
+import Skills from "./components/Skills"
 import Layout from "./layout/Layout"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 			<Layout>
 				<Header />
 				<About />
+				<Skills />
 			</Layout>
 		</>
 	)
