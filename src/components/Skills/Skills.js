3920ff4cf5f4de81b3a213a18ed0be9fba769f36
DFalque <falque@me.com>
2021-10-17 19:38:56 +0200

    add skills section

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,19 @@
+import "./Skills.scss"
+
+const Skills = () => {
+	return (
+		<section className="Skills">
+			<h2>Skills</h2>
+			<div className="Skills__container">
+				<div className="Skills__Skill"></div>
+				<div className="Skills__Skill"></div>
+				<div className="Skills__Skill"></div>
+				<div className="Skills__Skill"></div>
+				<div className="Skills__Skill"></div>
+				<div className="Skills__Skill"></div>
+			</div>
+		</section>
+	)
+}
+
+export default Skills
