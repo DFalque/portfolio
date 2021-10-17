ac5c08078bab6835f64186a278c7534c8a2f5b71
DFalque <falque@me.com>
2021-10-17 19:13:57 +0200

    change style Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,6 +5,7 @@ function Nav() {
 			<a className="Nav__icon">Diego Falque</a>
 			<nav className="Nav__LinksContainer">
 				<a className="Nav__LinksContainer__Link">About me</a>
+				<a className="Nav__LinksContainer__Link">Skills</a>
 				<a className="Nav__LinksContainer__Link">Work</a>
 				<a className="Nav__LinksContainer__Link">Contact</a>
 			</nav>
