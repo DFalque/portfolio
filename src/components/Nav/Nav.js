9246af7b9aa3c78fe21adfff702c32b7751a2cfc
DFalque <falque@me.com>
2021-10-18 12:24:20 +0200

    add href to the Nav Links

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,51 @@
 import "./Nav.scss"
 function Nav() {
+	const scroll = (e) => {
+		e.preventDefault()
+		const target = e.target.getAttribute("href")
+		//const element = document.querySelector(target)
+		const location = document.querySelector(target).offsetTop
+		window.scrollTo({
+			left: 0,
+			top: location - 50,
+			behavior: "smooth",
+		})
+		console.log(location)
+		//const link = 'document.getElementById("skills")'
+		//window.scrollTo(0, link)
+	}
 	return (
-		<div className="Nav">
+		<div className="Nav sticky">
 			<a className="Nav__icon">Diego Falque</a>
 			<nav className="Nav__LinksContainer">
-				<a className="Nav__LinksContainer__Link">About me</a>
-				<a className="Nav__LinksContainer__Link">Skills</a>
-				<a className="Nav__LinksContainer__Link">Work</a>
-				<a className="Nav__LinksContainer__Link">Contact</a>
+				<a
+					href="#about"
+					onClick={(e) => scroll(e)}
+					className="Nav__LinksContainer__Link"
+				>
+					About
+				</a>
+				<a
+					className="Nav__LinksContainer__Link"
+					href="#skills"
+					onClick={(e) => scroll(e)}
+				>
+					Skills
+				</a>
+				<a
+					className="Nav__LinksContainer__Link"
+					href="#work"
+					onClick={(e) => scroll(e)}
+				>
+					Work
+				</a>
+				<a
+					className="Nav__LinksContainer__Link"
+					href="#contact"
+					onClick={(e) => scroll(e)}
+				>
+					Contact
+				</a>
 			</nav>
 		</div>
 	)
