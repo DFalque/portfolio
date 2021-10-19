75c40ca30f77c6a4c8b6a97fdb9429c4a9bfde6d
DFalque <falque@me.com>
2021-10-19 20:12:11 +0200

    create data

---
+++ src/data/data.js
@@ -0,0 +1,19 @@
+export const nav = ["about", "skills", "works"]
+
+export const header = {
+	title: "Este es el titulo",
+}
+
+export const about = {
+	info: "Esta es la información",
+}
+
+export const work = [
+	{ id: 1, description: "Esta es la descripción" },
+	{ id: 1, description: "Esta es la descripción" },
+]
+
+export const skill = [
+	{ id: 1, description: "Esta es la descripción" },
+	{ id: 1, description: "Esta es la descripción" },
+]
