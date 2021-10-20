4aa75eb082a8a383b3b042bf00518041074251cf
DFalque <falque@me.com>
2021-10-20 13:44:53 +0200

    chaange About style

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,30 +1,33 @@
 import "./About.scss"
-import Image from "../../assets/images/aaaa.jpeg"
+import Image from "../../assets/images/me.JPG"
 
 const About = () => {
 	return (
 		<section className="About" id="about">
-			<div className="About__text">
-				<h2>About me</h2>
-				<p>
-					Graduated in “Software engineering” & “Business Administration”. I
-					really ️work with Javascript and Python. I started my professional
-					career in the management area but I switched to the Software Sector
-					when I saw what we could do with a computer ‍ .
-				</p>
-				<p>
-					Graduated in “Software engineering” & “Business Administration”. I
-					really ️work with Javascript and Python. I started my professional
-					career in the management area but I switched to the Software Sector
-					when I saw what we could do with a computer ‍ .
-				</p>
-				<p>
-					Graduated in “Software engineering” & “Business Administration”. I
-					really ️work with Javascript and Python.
-				</p>
-			</div>
-			<div className="About__image">
-				<img src={Image} />
+			<div className="About__content">
+				<div className="About__text">
+					<h2>About me</h2>
+					<p>
+						I started my professional career in the management and accounting
+						area because I had studied Business Administration. One of my first
+						jobs was Project assistant in a technology institution, where I
+						worked on different projects related to government subsidy and
+						market analysis to tech startups
+					</p>
+					<p>
+						I had clear that I wanted to increase my technology knowledge and
+						start to work in that area. That was the reason because I became to
+						study Software engineer.
+					</p>
+					<p>
+						As a programmer I have specialized in Javascript / Typescript
+						language deu to its popularity and flexibility and I work as a React
+						developer (React native, Gastby, etc).
+					</p>
+				</div>
+				<div className="About__imageContainer">
+					<img className="image" src={Image}></img>
+				</div>
 			</div>
 		</section>
 	)
