16140177bd93edf84438afed1f116a1a8d4ca617
DFalque <falque@me.com>
2021-10-22 19:35:22 +0200

    edit footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,52 @@
 import "./Footer.scss"
+
+import { ReactComponent as ImgGithub } from "../../assets/images/github.svg"
+import { ReactComponent as ImgDribbble } from "../../assets/images/dribbble.svg"
+import { ReactComponent as ImgInstagram } from "../../assets/images/instagram.svg"
+import { ReactComponent as ImgLinkdlin } from "../../assets/images/dribbble.svg"
+
+//const imgGithub = "../../assets/images/github.svg"
 function Footer() {
 	return (
 		<div className="Footer" id="contact">
 			<h2>Contact</h2>
 			<h3>Let's create your next experience together</h3>
-			<a>hello@chetanverma.com</a>
-			<div>
-				<a>Icono</a>
-				<a>Icono</a>
-				<a>Icono</a>
-				<a>Icono</a>
+			<a className="email-link" href={`mailto:falque@me.com`}>
+				falque@me.com
+			</a>
+			<div className="Footer__iconContainer">
+				<a
+					className="Footer__iconContainer__icon"
+					href="#"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<ImgGithub />
+				</a>
+				<a
+					className="Footer__iconContainer__icon"
+					href="#"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<ImgDribbble />
+				</a>
+				<a
+					className="Footer__iconContainer__icon"
+					href="#"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<ImgInstagram />
+				</a>
+				<a
+					className="Footer__iconContainer__icon"
+					href="#"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<ImgDribbble />
+				</a>
 			</div>
 		</div>
 	)
