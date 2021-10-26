4d0e52291770efb2f4c2a40e946cfefd941fa66e
DFalque <falque@me.com>
2021-10-26 12:37:42 +0200

    refactor and Add style to CardWork

--- src/components/CardWork/CardWork.js
+++ src/components/CardWork/CardWork.js
@@ -1,5 +1,19 @@
+import "./CardWork.scss"
+import Tag from "../interface/Tag"
+
 function CardWork() {
-	return <div>CardWork</div>
+	return (
+		<div className="CardWork">
+			<div className="CardWork__image"></div>
+			<h3>InstaClone</h3>
+
+			<div className="CardWork__tags">
+				<Tag name="Sass" tagId="Sass" />
+				<Tag name="Node" tagId="Node" />
+				<Tag name="React" tagId="React" />
+			</div>
+		</div>
+	)
 }
 
 export default CardWork
