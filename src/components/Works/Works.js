b7293bc3337e73cdd3e456a7770dfd51d834e32e
DFalque <falque@me.com>
2021-10-26 12:37:16 +0200

    change Work Section responsive

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -1,15 +1,17 @@
+import CardWork from "../CardWork/CardWork"
 import "./Works.scss"
+
 const Works = () => {
 	return (
 		<section className="Works" id="work">
 			<h2>Works</h2>
 			<div className="Works__container">
-				<div className="Works__Work"></div>
-				<div className="Works__Work"></div>
-				<div className="Works__Work"></div>
-				<div className="Works__Work"></div>
-				<div className="Works__Work"></div>
-				<div className="Works__Work"></div>
+				<CardWork />
+				<CardWork />
+				<CardWork />
+				<CardWork />
+				<CardWork />
+				<CardWork />
 			</div>
 		</section>
 	)
