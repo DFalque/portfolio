d550951129a84fa5bd516f0b870952bd60c76eab
DFalque <falque@me.com>
2021-11-22 21:03:18 +0000

    changes in the data

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,6 @@
+//IMAGES
+import InstaWork from "../assets/images/backgroundWorks/instaFondo.jpg"
+
 export const nav = ["about", "skills", "works"]
 
 export const header = {
@@ -9,8 +12,56 @@ export const about = {
 }
 
 export const work = [
-	{ id: 1, description: "Esta es la descripción" },
-	{ id: 1, description: "Esta es la descripción" },
+	{
+		id: 0,
+		img: InstaWork,
+		title: "InstaClone",
+		review: "A copy of Instagram app",
+		description: "Esta es la descripción",
+		tags: ["SASS", "REACT", "NODE"],
+
+		tags: ["SASS", "REACT", "NODE"],
+	},
+	{
+		id: 1,
+		img: InstaWork,
+		title: "InstaClone",
+		review: "A copy of Instagram app",
+		description: "Esta es la descripción",
+		tags: ["SASS", "REACT", "NODE"],
+	},
+	{
+		id: 2,
+		img: InstaWork,
+		title: "Mombo",
+		review: "A copy of Instagram app",
+		description: "Esta es la descripción",
+		tags: ["SASS", "REACT", "NODE"],
+	},
+	{
+		id: 3,
+		img: InstaWork,
+		title: "Clothes APP",
+		review: "A copy of Instagram app",
+		description: "Esta es la descripción",
+		tags: ["SASS", "REACT", "NODE"],
+	},
+	{
+		id: 4,
+		img: InstaWork,
+		title: "AliDrive",
+		review: "A copy of Instagram app",
+		description: "Esta es la descripción",
+		tags: ["SASS", "REACT", "NODE"],
+	},
+	{
+		id: 5,
+		img: InstaWork,
+		title: "AliDrive",
+		review: "A copy of Instagram app",
+		description: "Esta es la descripción",
+		tags: ["SASS", "REACT", "NODE"],
+	},
 ]
 
 export const skill = [
